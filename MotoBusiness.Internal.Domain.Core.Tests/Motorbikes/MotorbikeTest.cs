2ad06d6cf8c044b6ac1ff8469c5dc984c76699fc
MAS INOVAÇÕES,LDA <suporte@masinovacoes.com>
2024-10-04 12:28:13 +0100

    Adição de testes

---
+++ MotoBusiness.Internal.Domain.Core.Tests/Motorbikes/MotorbikeTest.cs
@@ -0,0 +1,25 @@
+﻿using MotoBusiness.Internal.Application.Motorbikes;
+
+namespace MotoBusiness.Internal.Domain.Core.Tests.Motorbikes;
+
+public class MotorbikeTest
+{
+    [Theory(DisplayName ="Verifica se o ano de fabrico é 2024")]
+    [InlineData(2023, false)]
+    [InlineData(2024, true)]
+    [InlineData(2015, false)]
+    [InlineData(2022, false)]
+    public void ChecksIfTheYearOfManufactureIs2024ShouldReturnTrue(
+        short year, bool expected)
+    {
+        //Arrange
+        var request = new MotorbikeRegisterRequest("Honda", "ABC1234", year);
+
+        //Action
+        var motorbike = request.Convert();
+        var response = motorbike.IsManufactureYears2024();
+
+        //Assert
+        Assert.Equal(expected, response);
+    }
+}
